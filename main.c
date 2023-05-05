e9ad7388ffaeb1c9068928ec2ea7ac0b236d6cba
Zuo <chiushengzuo@gmail.com>
2023-05-05 17:34:51 +0800

    Edit variable name

--- main.c
+++ main.c
@@ -1,65 +1,75 @@
 #include <stdio.h>
 
 int main() {
-    char str='0';
-    float GPA,gradePoint,sum;
-    int credit,total_credit=0;
-    do{
-        if(str<'1'||str>'9')scanf("%c",&str);
-        if(str>='1'&&str<='9') {
-            credit = str - '0';
-            total_credit+=credit;
-            scanf("%c",&str);
-            if(str>='A'&&str<='F'){
-                switch (str) {
+    //Example input: 1A+3B2A+
+    char perCharInString = '0';
+    float GPA, gradePoint, sum;
+    int credit, totalCredit = 0;
+
+    do {
+        if (perCharInString < '1' || perCharInString > '9')
+            scanf("%c", &perCharInString);
+        if (perCharInString >= '1' && perCharInString <= '9') {
+            credit = perCharInString - '0';
+            totalCredit += credit;
+            scanf("%c", &perCharInString);
+            if (perCharInString >= 'A' && perCharInString <= 'F') {
+                switch (perCharInString) {
                     case 'A':
-                        scanf("%c",&str);
-                        if(str=='+') {
+                        scanf("%c", &perCharInString);
+                        if (perCharInString == '+') {
                             gradePoint = 4.3f;
-                            sum+=(credit*gradePoint);
-                        }
-                        else if(str=='-') {
+                            sum += (credit * gradePoint);
+                        } else if (perCharInString == '-') {
                             gradePoint = 3.7f;
-                            sum+=(credit*gradePoint);
+                            sum += (credit * gradePoint);
+                        } else {
+                            gradePoint = 4.0f;
+                            sum += (credit * gradePoint);
                         }
-                        else {gradePoint=4.0f;sum+=(credit*gradePoint);}
                         break;
                     case 'B':
                         //printf("b ");
-                        scanf("%c",&str);
-                        if(str=='+') {
+                        scanf("%c", &perCharInString);
+                        if (perCharInString == '+') {
                             gradePoint = 3.3f;
-                            sum+=(credit*gradePoint);
-                        }
-                        else if(str=='-') {
+                            sum += (credit * gradePoint);
+                        } else if (perCharInString == '-') {
                             gradePoint = 2.7f;
-                            sum+=(credit*gradePoint);
-                        }
-                        else {gradePoint=3.0f;sum+=(credit*gradePoint);}
-                        break;
-                    case 'C'://printf("c ");
-                        scanf("%c",&str);
-                        if(str=='+') {
-                            gradePoint = 2.3f;
-                            sum+=(credit*gradePoint);
-                        }
-                        else if(str=='-') {
-                            gradePoint = 1.7f;
+                            sum += (credit * gradePoint);
+                        } else {
+                            gradePoint = 3.0f;
                             sum += (credit * gradePoint);
                         }
-                        else {gradePoint=2.0f;sum+=(credit*gradePoint);}
                         break;
-                    case 'F'://printf("f ");
-                        gradePoint=0.0f;
-                        sum+=0;
+                    case 'C':
+                        //printf("c ");
+                        scanf("%c", &perCharInString);
+                        if (perCharInString == '+') {
+                            gradePoint = 2.3f;
+                            sum += (credit * gradePoint);
+                        } else if (perCharInString == '-') {
+                            gradePoint = 1.7f;
+                            sum += (credit * gradePoint);
+                        } else {
+                            gradePoint = 2.0f;
+                            sum += (credit * gradePoint);
+                        }
+                        break;
+                    case 'F':
+                        //printf("f ");
+                        gradePoint = 0.0f;
+                        sum += 0;
                         break;
                 }
             }
         }
-    }while(str!='\n');
+    } while (perCharInString != '\n');
     //printf("%d %f\n",total_credit,sum);
-    if(total_credit==0)printf("0.00");
-    else{GPA=sum/total_credit;
-    printf("%.2f",GPA);}
+    if (totalCredit == 0)printf("0.00");
+    else {
+        GPA = sum / totalCredit;
+        printf("%.2f", GPA);
+    }
     return 0;
 }
